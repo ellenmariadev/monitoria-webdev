97b6ecb8ff8b2d588305fc8ca109eced1c74babb
ellenmariadev <ellenmariadev@gmail.com>
2023-02-06 23:47:15 -0300

    docs: swagger documentation

--- src/server.js
+++ src/server.js
@@ -2,10 +2,17 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
 import { PORT } from "./config/database.js";
+import swaggerUi from "swagger-ui-express";
+// import swaggerDocument from "../swagger.json";
+
+import { createRequire } from "module";          
+const require = createRequire(import.meta.url);
+const swaggerDocument = require("../swagger.json");  
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use(routes);
 app.listen(PORT, () =>
