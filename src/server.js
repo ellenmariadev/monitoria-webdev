c7f8e217f2e78cd4e6bdc6457bd758aa07189fd4
ellenmariadev <ellenmariadev@gmail.com>
2023-02-07 00:00:12 -0300

    docs: redocly documentation

--- src/server.js
+++ src/server.js
@@ -3,16 +3,21 @@ import cors from "cors";
 import routes from "./routes.js";
 import { PORT } from "./config/database.js";
 import swaggerUi from "swagger-ui-express";
-// import swaggerDocument from "../swagger.json";
 
-import { createRequire } from "module";          
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const swaggerDocument = require("../swagger.json");  
+const swaggerDocument = require("../swagger.json");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/swagger", (req, res) => {
+  return res.sendFile(process.cwd() + "/swagger.json");
+});
+app.get("/docs", (req, res) => {
+  return res.sendFile(process.cwd() + "/index.html");
+});
 app.use(cors());
 app.use(routes);
 app.listen(PORT, () =>
